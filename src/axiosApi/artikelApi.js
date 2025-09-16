cdb9f3695adbf6f7b3816d42c2889511824dd53f
ralir13 <rali.ramdhani1313@gmail.com>
2025-09-16 12:22:39 +0700

    add axiosApi di artikel

--- src/axiosApi/artikelApi.js
+++ src/axiosApi/artikelApi.js
@@ -3,24 +3,44 @@ import api from "./apiConfig";
 
 // GET
 export async function getArticles() {
-  const res = await api.get("artikel/");
-  return res.data.results || res.data;
+  try {
+    const res = await api.get("artikel/");
+    return res.data.results || res.data;
+  } catch (error) {
+    console.error("Gagal fetch artikel:", error);
+    throw error;
+  }
 }
 
 // POST
 export async function createArticle(data) {
-  const res = await api.post("artikel/", data);
-  return res.data;
+  try {
+    const res = await api.post("artikel/", data);
+    return res.data;
+  } catch (error) {
+    console.error("Gagal membuat artikel:", error);
+    throw error;
+  }
 }
 
 // PUT
 export async function updateArticle(id, data) {
-  const res = await api.put(`artikel/${id}/`, data);
-  return res.data;
+  try {
+    const res = await api.put(`artikel/${id}/`, data);
+    return res.data;
+  } catch (error) {
+    console.error("Gagal update artikel:", error);
+    throw error;
+  }
 }
 
 // DELETE
 export async function deleteArticle(id) {
-  await api.delete(`artikel/${id}/`);
-  return true;
+  try {
+    await api.delete(`artikel/${id}/`);
+    return true;
+  } catch (error) {
+    console.error("Gagal hapus artikel:", error);
+    throw error;
+  }
 }
